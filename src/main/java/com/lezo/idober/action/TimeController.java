bfd26149c8f33cefed23a08e800bae232f3526f6
lcstore <lcstore@126.com>
2016-11-01 21:00:45 +0800

    rm bttiantang

--- src/main/java/com/lezo/idober/action/TimeController.java
+++ src/main/java/com/lezo/idober/action/TimeController.java
@@ -1,18 +1,33 @@
 package com.lezo.idober.action;
 
+import java.util.Collection;
+
 import lombok.extern.log4j.Log4j;
 
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.solr.client.solrj.SolrQuery;
+import org.apache.solr.client.solrj.SolrQuery.ORDER;
+import org.apache.solr.client.solrj.SolrServer;
+import org.apache.solr.client.solrj.response.QueryResponse;
+import org.apache.solr.common.SolrDocument;
+import org.apache.solr.common.SolrDocumentList;
+import org.jsoup.Connection.Method;
+import org.jsoup.Connection.Response;
+import org.jsoup.Jsoup;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 import com.lezo.idober.timer.AssembleIdMovieTimer;
 import com.lezo.idober.timer.FillTorrent2MovieTimer;
 import com.lezo.idober.timer.OnlineTorrentMovieTimer;
 import com.lezo.idober.timer.QueryTorrent4NewMovieTimer;
 import com.lezo.idober.timer.UnifyRegionTimer;
+import com.lezo.idober.utils.SolrUtils;
 
 @Log4j
 @Controller
@@ -28,6 +43,100 @@ public class TimeController extends BaseController {
     private QueryTorrent4NewMovieTimer queryTorrent4NewMovieTimer;
     @Autowired
     private AssembleIdMovieTimer assembleIdMovieTimer;
+
+    @ResponseBody
+    @RequestMapping(value = { "fixtorrent" }, method = RequestMethod.GET)
+    public String fixBt() throws Exception {
+        SolrServer movieServer = SolrUtils.getSolrServer(SolrUtils.CORE_ONLINE_MOVIE);
+        int limit = 100;
+        String fromId = "0";
+        while (true) {
+            SolrDocumentList selectDocs = null;
+            try {
+                selectDocs = getMovieByIdWithLimit(movieServer, fromId, limit);
+            } catch (Exception e) {
+                log.warn("", e);
+            }
+            if (selectDocs == null) {
+                break;
+            }
+            SolrDocumentList newDocumentList = new SolrDocumentList();
+            for (SolrDocument inDoc : selectDocs) {
+                Collection<Object> torrents = inDoc.getFieldValues("torrents");
+                if (CollectionUtils.isEmpty(torrents)) {
+                    continue;
+                }
+                boolean bError = false;
+                for (Object tor : torrents) {
+                    if (tor.toString().contains("bttiantang")) {
+                        bError = true;
+                        break;
+                    }
+                }
+                if (bError) {
+                    newDocumentList.add(inDoc);
+                }
+            }
+            newQueryTasks(newDocumentList);
+            if (selectDocs.size() < limit) {
+                break;
+            } else {
+                for (SolrDocument doc : selectDocs) {
+                    String curId = doc.getFieldValue("id").toString();
+                    if (fromId.compareTo(curId) < 0) {
+                        fromId = curId;
+                    }
+                }
+            }
+        }
+        return "OK";
+    }
+
+    private void newQueryTasks(SolrDocumentList selectDocs) throws Exception {
+        if (CollectionUtils.isEmpty(selectDocs)) {
+            return;
+        }
+        JSONArray taskList = new JSONArray();
+        for (SolrDocument doc : selectDocs) {
+            String sId = doc.getFieldValue("id").toString();
+            String sName = doc.getFieldValue("name").toString();
+            JSONObject taskObj = new JSONObject();
+            taskObj.put("type", "query-movie");
+            taskObj.put("url", "");
+            taskObj.put("level", 1000);
+            JSONObject argsObj = new JSONObject();
+            argsObj.put("title", sName);
+            argsObj.put("mid", sId);
+            argsObj.put("retry", "0");
+            taskObj.put("args", argsObj);
+            taskList.add(taskObj);
+        }
+        createTasks(taskList);
+    }
+
+    private void createTasks(JSONArray taskArray) throws Exception {
+        if (taskArray == null || taskArray.size() < 1) {
+            return;
+        }
+        String url = "http://www.lezomao.com:8090/taskmgr/createtasks";
+        String referrer = "http://www.lezomao.com/idober";
+        Response resp =
+                Jsoup.connect(url).referrer(referrer).method(Method.POST).data("tasks", taskArray.toJSONString())
+                        .ignoreContentType(true).execute();
+        log.info("task:" + taskArray.size() + ",resp:" + resp.body());
+    }
+
+    private SolrDocumentList getMovieByIdWithLimit(SolrServer movieServer, String fromId, int limit)
+            throws Exception {
+        SolrQuery solrQuery = new SolrQuery();
+        solrQuery.setStart(0);
+        solrQuery.setRows(limit);
+        solrQuery.set("q", "id:[" + fromId + " TO *]");
+        solrQuery.addSort("id", ORDER.asc);
+        // solrQuery.set("q", "id:557215591");
+        QueryResponse resp = movieServer.query(solrQuery);
+        return resp.getResults();
+    }
 
     @ResponseBody
     @RequestMapping(value = { "torrent" },
--- src/test/java/com/lezo/idober/timer/TimerTest.java
+++ src/test/java/com/lezo/idober/timer/TimerTest.java
@@ -8,29 +8,29 @@ import com.lezo.idober.solr.BaseTest;
 import com.lezo.iscript.spring.context.SpringBeanUtils;
 
 public class TimerTest extends BaseTest {
-	@Autowired
-	private FillTorrent2MovieTimer fillTorrent2MovieTimer;
-	@Autowired
-	private FillOldId2MovieTimer fillOldId2MovieTimer;
-	@Autowired
-	private UnifyRegionTimer unifyRegionTimer;
-	@Autowired
-	private OnlineTorrentMovieTimer onlineTorrentMovieTimer;
-	@Autowired
-	private AssembleIdMovieTimer assembleIdMovieTimer;
-	@Autowired
-	private QueryTorrent4NewMovieTimer queryTorrent4NewMovieTimer;
+    @Autowired
+    private FillTorrent2MovieTimer fillTorrent2MovieTimer;
+    @Autowired
+    private FillOldId2MovieTimer fillOldId2MovieTimer;
+    @Autowired
+    private UnifyRegionTimer unifyRegionTimer;
+    @Autowired
+    private OnlineTorrentMovieTimer onlineTorrentMovieTimer;
+    @Autowired
+    private AssembleIdMovieTimer assembleIdMovieTimer;
+    @Autowired
+    private QueryTorrent4NewMovieTimer queryTorrent4NewMovieTimer;
 
-	@Test
-	public void testTimer() {
-		AppConfig appConfig = SpringBeanUtils.getBean(AppConfig.class);
-		appConfig.setEnvName("dev");
-//		fillTorrent2MovieTimer.run();
-		// fillOldId2MovieTimer.run();
-		// unifyRegionTimer.run();
-//		 onlineTorrentMovieTimer.run();
-		 assembleIdMovieTimer.run();
-		// queryTorrent4NewMovieTimer.run();
-	}
+    @Test
+    public void testTimer() {
+        AppConfig appConfig = SpringBeanUtils.getBean(AppConfig.class);
+        appConfig.setEnvName("dev");
+        fillTorrent2MovieTimer.run();
+        // fillOldId2MovieTimer.run();
+        // unifyRegionTimer.run();
+        // onlineTorrentMovieTimer.run();
+        // assembleIdMovieTimer.run();
+        // queryTorrent4NewMovieTimer.run();
+    }
 
 }
