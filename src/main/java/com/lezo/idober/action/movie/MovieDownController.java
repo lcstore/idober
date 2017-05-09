378c606c0a43d61202f34995468a4336d9086a71
lcstore <lcstore@126.com>
2017-05-09 13:27:49 +0800

    add notice

--- src/main/java/com/lezo/idober/action/movie/MovieDownController.java
+++ src/main/java/com/lezo/idober/action/movie/MovieDownController.java
@@ -1,8 +1,6 @@
 package com.lezo.idober.action.movie;
 
 import java.io.File;
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 import java.util.List;
 
@@ -28,7 +26,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -44,149 +41,151 @@ import com.lezo.iscript.rest.http.HttpClientManager;
 @Controller
 @RequestMapping("movie")
 public class MovieDownController extends BaseController {
-	private static final String DEFAULT_CHARSET = "UTF-8";
-	private static final DefaultHttpClient HTTP_CLIENT = HttpClientManager.getDefaultHttpClient();
-	@Autowired
-	private AppConfig config;
+    private static final String DEFAULT_CHARSET = "UTF-8";
+    private static final DefaultHttpClient HTTP_CLIENT = HttpClientManager.getDefaultHttpClient();
+    @Autowired
+    private AppConfig config;
 
-	@RequestMapping("download")
-	public ResponseEntity<byte[]> download(@RequestParam(value = "u", required = false) String url,
-			@RequestParam("p") String params,
-			@RequestParam(value = "n", required = false) String sName,
-			@RequestParam(value = "m", defaultValue = "G") String sMethod,
-			@RequestParam(value = "id") String id,
-			@RequestParam(value = "tid", defaultValue = "") String torrentId
-			)
-					throws Exception {
-		if (StringUtils.isBlank(url)) {
-			url = "http://www.bttiantang.com/download3.php";
-		} else {
-			url = URLDecoder.decode(url, DEFAULT_CHARSET);
-		}
-		HttpUriRequest request = null;
-		if ("P".equals(sMethod.toUpperCase())) {
-			HttpPost post = new HttpPost(url);
-			if (StringUtils.isNotEmpty(params)) {
-				params = URLDecoder.decode(params, DEFAULT_CHARSET);
-				params = params.replaceAll("&amp;", "&");
-				params = params.replaceAll("ℑField", "&imageField");
-				params = params.replaceAll("∾tion", "&action");
-				if (url.contains(".bttiantang.com")) {
-					List<NameValuePair> paramList = Lists.newArrayList();
-					String[] paramArr = params.split("&");
-					for (String param : paramArr) {
-						String[] kvArr = param.split("=");
-						int index = -1;
-						String key = kvArr[++index];
-						String value = kvArr[++index];
-						NameValuePair nvPair = new BasicNameValuePair(key, value);
-						paramList.add(nvPair);
-					}
-					UrlEncodedFormEntity entity = new UrlEncodedFormEntity(paramList, DEFAULT_CHARSET);
-					post.setEntity(entity);
-				} else {
-					HttpEntity entity = new StringEntity(params, DEFAULT_CHARSET);
-					post.setEntity(entity);
-				}
-			}
-			request = post;
-		} else {
-			if (StringUtils.isNotBlank(params)) {
-				params = URLDecoder.decode(params, DEFAULT_CHARSET);
-				url += url.contains("?") ? params : "?" + params;
-				params = "";
-			}
-			request = new HttpGet(url);
-		}
-		if (url.contains(".rarbt.com")) {
-			request.setHeader("Content-Type", "application/x-www-form-urlencoded");
-		}
-		torrentId = createTorrentId(url, params, torrentId);
-		JSONObject torrentObject = loadTorrents(id, torrentId);
-		if (torrentObject == null) {
-			torrentObject = executeRequest(request, url, sName);
-			saveTorrentFile(id, torrentId, torrentObject);
-		}
-		byte[] dataBytes = torrentObject.getBytes("raw");
-		HttpHeaders headers = new HttpHeaders();
-		headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
-		String sFileName = torrentObject.getString("name");
-		sFileName = new String(sFileName.getBytes(), "iso-8859-1");
-		headers.setContentDispositionFormData("attachment", sFileName);
-		return new ResponseEntity<byte[]>(dataBytes, headers, HttpStatus.OK);
-	}
+    @RequestMapping("download")
+    public ResponseEntity<byte[]> download(@RequestParam(value = "u", required = false) String url,
+            @RequestParam("p") String params,
+            @RequestParam(value = "n", required = false) String sName,
+            @RequestParam(value = "m", defaultValue = "G") String sMethod,
+            @RequestParam(value = "id") String id,
+            @RequestParam(value = "tid", defaultValue = "") String torrentId
+            )
+                    throws Exception {
+        if (StringUtils.isBlank(url)) {
+            url = "http://www.bttiantang.com/download3.php";
+        } else {
+            url = URLDecoder.decode(url, DEFAULT_CHARSET);
+        }
+        HttpUriRequest request = null;
+        if ("P".equals(sMethod.toUpperCase())) {
+            HttpPost post = new HttpPost(url);
+            if (StringUtils.isNotEmpty(params)) {
+                params = URLDecoder.decode(params, DEFAULT_CHARSET);
+                params = params.replaceAll("&amp;", "&");
+                params = params.replaceAll("ℑField", "&imageField");
+                params = params.replaceAll("∾tion", "&action");
+                if (url.contains(".bttiantang.com")) {
+                    List<NameValuePair> paramList = Lists.newArrayList();
+                    String[] paramArr = params.split("&");
+                    for (String param : paramArr) {
+                        String[] kvArr = param.split("=");
+                        int index = -1;
+                        String key = kvArr[++index];
+                        String value = kvArr[++index];
+                        NameValuePair nvPair = new BasicNameValuePair(key, value);
+                        paramList.add(nvPair);
+                    }
+                    UrlEncodedFormEntity entity = new UrlEncodedFormEntity(paramList, DEFAULT_CHARSET);
+                    post.setEntity(entity);
+                } else {
+                    HttpEntity entity = new StringEntity(params, DEFAULT_CHARSET);
+                    post.setEntity(entity);
+                }
+            }
+            request = post;
+        } else {
+            if (StringUtils.isNotBlank(params)) {
+                params = URLDecoder.decode(params, DEFAULT_CHARSET);
+                url += url.contains("?") ? params : "?" + params;
+                params = "";
+            }
+            request = new HttpGet(url);
+        }
+        if (url.contains(".rarbt.com")) {
+            request.setHeader("Content-Type", "application/x-www-form-urlencoded");
+        }
+        request.setHeader("User-Agent",
+                "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.10; rv:53.0) Gecko/20100101 Firefox/53.0");
+        torrentId = createTorrentId(url, params, torrentId);
+        JSONObject torrentObject = loadTorrents(id, torrentId);
+        if (torrentObject == null) {
+            torrentObject = executeRequest(request, url, sName);
+            saveTorrentFile(id, torrentId, torrentObject);
+        }
+        byte[] dataBytes = torrentObject.getBytes("raw");
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+        String sFileName = torrentObject.getString("name");
+        sFileName = new String(sFileName.getBytes(), "iso-8859-1");
+        headers.setContentDispositionFormData("attachment", sFileName);
+        return new ResponseEntity<byte[]>(dataBytes, headers, HttpStatus.OK);
+    }
 
-	private void saveTorrentFile(String id, String torrentId, JSONObject torrentObject) throws Exception {
-		if (torrentObject == null || StringUtils.isBlank(id) || StringUtils.isBlank(torrentId)) {
-			return;
-		}
-		File torrentFile = new File(config.getTorrentDir(), id + File.separator + torrentId + ".data");
-		if (torrentFile.exists()) {
-			return;
-		}
-		if (torrentFile.getParentFile() != null) {
-			torrentFile.getParentFile().mkdirs();
-		}
-		FileUtils.writeStringToFile(torrentFile, torrentObject.toJSONString(), DEFAULT_CHARSET);
-	}
+    private void saveTorrentFile(String id, String torrentId, JSONObject torrentObject) throws Exception {
+        if (torrentObject == null || StringUtils.isBlank(id) || StringUtils.isBlank(torrentId)) {
+            return;
+        }
+        File torrentFile = new File(config.getTorrentDir(), id + File.separator + torrentId + ".data");
+        if (torrentFile.exists()) {
+            return;
+        }
+        if (torrentFile.getParentFile() != null) {
+            torrentFile.getParentFile().mkdirs();
+        }
+        FileUtils.writeStringToFile(torrentFile, torrentObject.toJSONString(), DEFAULT_CHARSET);
+    }
 
-	private JSONObject executeRequest(HttpUriRequest request, String url, String sName) throws Exception {
-		String userAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.10; rv:47.0) Gecko/20100101 Firefox/47.0";
-		String referrer = url;
-		request.setHeader("Referer", referrer);
-		request.setHeader("User-Agent", userAgent);
-		HttpResponse resp = HTTP_CLIENT.execute(request);
+    private JSONObject executeRequest(HttpUriRequest request, String url, String sName) throws Exception {
+        String userAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.10; rv:47.0) Gecko/20100101 Firefox/47.0";
+        String referrer = url;
+        request.setHeader("Referer", referrer);
+        request.setHeader("User-Agent", userAgent);
+        HttpResponse resp = HTTP_CLIENT.execute(request);
 
-		String disKey = "Content-Disposition";
-		Header destHeader = resp.getLastHeader(disKey);
-		String attach = destHeader == null ? null : destHeader.getValue();
-		String fileName = attach;
-		String sDomain = "lezomao.com-";
-		if (StringUtils.isNotBlank(sName)) {
-			sName = sName.endsWith(".torrent") ? sName : sName + ".torrent";
-			sName = sName.replaceAll("\\s{2,}", "");
-			sName = sDomain + sName;
-			// sName = URLEncoder.encode(sName, "UTF-8");
-			fileName = sName;
-		} else if (StringUtils.isNotBlank(attach)) {
-			fileName = sDomain + sName;
-		} else {
-			fileName = sDomain + System.currentTimeMillis() + ".torrent";
-		}
-		JSONObject tObject = null;
-		if (resp.getStatusLine().getStatusCode() == HttpStatus.OK.value()) {
-			byte[] dataBytes = EntityUtils.toByteArray(resp.getEntity());
-			tObject = new JSONObject();
-			tObject.put("raw", dataBytes);
-			tObject.put("name", fileName);
-		}
-		return tObject;
-	}
+        String disKey = "Content-Disposition";
+        Header destHeader = resp.getLastHeader(disKey);
+        String attach = destHeader == null ? null : destHeader.getValue();
+        String fileName = attach;
+        String sDomain = "lezomao.com-";
+        if (StringUtils.isNotBlank(sName)) {
+            sName = sName.endsWith(".torrent") ? sName : sName + ".torrent";
+            sName = sName.replaceAll("\\s{2,}", "");
+            sName = sDomain + sName;
+            // sName = URLEncoder.encode(sName, "UTF-8");
+            fileName = sName;
+        } else if (StringUtils.isNotBlank(attach)) {
+            fileName = sDomain + sName;
+        } else {
+            fileName = sDomain + System.currentTimeMillis() + ".torrent";
+        }
+        JSONObject tObject = null;
+        if (resp.getStatusLine().getStatusCode() == HttpStatus.OK.value()) {
+            byte[] dataBytes = EntityUtils.toByteArray(resp.getEntity());
+            tObject = new JSONObject();
+            tObject.put("raw", dataBytes);
+            tObject.put("name", fileName);
+        }
+        return tObject;
+    }
 
-	private JSONObject loadTorrents(String id, String torrentId) {
-		File torrentFile = new File(config.getTorrentDir(), id + File.separator + torrentId + ".data");
-		JSONObject tObject = null;
-		if (torrentFile.exists()) {
-			try {
-				String sContent = FileUtils.readFileToString(torrentFile, DEFAULT_CHARSET);
-				tObject = JSONObject.parseObject(sContent);
-			} catch (Exception e) {
-				torrentFile.deleteOnExit();
-				log.warn("load id:" + id + ",tid:" + torrentId + ",cause:", e);
-			}
-		}
-		return tObject;
-	}
+    private JSONObject loadTorrents(String id, String torrentId) {
+        File torrentFile = new File(config.getTorrentDir(), id + File.separator + torrentId + ".data");
+        JSONObject tObject = null;
+        if (torrentFile.exists()) {
+            try {
+                String sContent = FileUtils.readFileToString(torrentFile, DEFAULT_CHARSET);
+                tObject = JSONObject.parseObject(sContent);
+            } catch (Exception e) {
+                torrentFile.deleteOnExit();
+                log.warn("load id:" + id + ",tid:" + torrentId + ",cause:", e);
+            }
+        }
+        return tObject;
+    }
 
-	private String createTorrentId(String url, String params, String torrentId) {
-		if (StringUtils.isBlank(torrentId)) {
-			Hasher hasher = Hashing.md5().newHasher();
-			hasher.putString(url);
-			if (params != null) {
-				hasher.putString(params);
-			}
-			torrentId = hasher.hash().toString();
-		}
-		return torrentId;
-	}
+    private String createTorrentId(String url, String params, String torrentId) {
+        if (StringUtils.isBlank(torrentId)) {
+            Hasher hasher = Hashing.md5().newHasher();
+            hasher.putString(url);
+            if (params != null) {
+                hasher.putString(params);
+            }
+            torrentId = hasher.hash().toString();
+        }
+        return torrentId;
+    }
 }
---
+++ src/main/webapp/WEB-INF/ftl/decorator/noticebar.ftl
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title><sitemesh:write property='title' /></title>
+    <sitemesh:write property='head'/>
+</head>
+<body>
+	<div class="notice">
+	  <div class="row text-center">
+	  <h3 style="margin-top: 15px;margin-bottom: 15px;">网站即将全面升级，电影电视剧尽在掌握，免费资源为你所用，敬请期待...</h3></div>
+	</div>
+    <sitemesh:write property='body'/>
+</body>
+</html>
